e6ad909a8b17c5e1b9d63c26fd3bf517f114bf6b
Sebastian Florian <rebardesigndk@gmail.com>
2017-12-04 22:12:21 +0100

    feature(Robot): Add joint constructor

--- robot-gui/src/generators/Robot.js
+++ robot-gui/src/generators/Robot.js
@@ -5,34 +5,51 @@ class Robot {
   robotBones = [];
   joints = [];
   colors = [0xaaaaaa, 0xbbbbbb, 0xbcbcbc, 0xcbcbcb, 0xcccccc, 0x0];
+  wireframe = false;
 
   constructor() {
     this.thickness = 1;
 
     // test only
-    const cube = this.createCube(0, 0, 0, 2, 2, 2, 0, 0, 0);
+    const cube = this.createBox(0, 0, 5, 1.5, 0.3, 5);
+    const base = this.createJoint(0, 0, 1, 2, 2, 2, 0, 0, 0);
+    const shoulder = this.createJoint(0, 0, 8, 1, 4, 2, 0, 0, 0);
+    base.rotation.x = (Math.PI / 2);
+    shoulder.rotation.z = (Math.PI / 2);
+
     this.group.add(cube);
+    this.group.add(base);
+    this.group.add(shoulder);
   }
 
-  createCube(x, y, z, w, h, d, min, max, jointNumber) {
+  createBox(x = 0, y = 0, z = 0, w = 1, h = 1, d = 1) {
     const wThickened = Math.abs(w) + this.thickness;
     const hThickened = Math.abs(h) + this.thickness;
     const dThickened = Math.abs(d) + this.thickness;
 
-    const material = new THREE.MeshLambertMaterial({ color: this.colors[jointNumber] });
+    const material = new THREE.MeshLambertMaterial({ color: 0xaaaaaa, wireframe: this.wireframe });
     const geometry = new THREE.CubeGeometry(
       wThickened,
       hThickened,
       dThickened,
     );
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
-
-    const cube = new THREE.Object3D();
+    const cube = new THREE.Mesh(geometry, material);
     cube.position.set(x, y, z);
-    cube.add(mesh);
 
     return cube;
+    // const arm = new THREE.Object3D();
+    // arm.position.set(x, y, z);
+    // arm.add(mesh);
+
+    // return arm;
+  }
+
+  createJoint(x, y, z, radius, h, d, min, max) {
+    const jointGeo1 = new THREE.CylinderGeometry(radius, radius, h, 24, 2, false, -min, (2 * Math.PI) - (max + min));
+    const joint = new THREE.Mesh(jointGeo1, new THREE.MeshBasicMaterial({ color: 0xffbb00, wireframe: this.wireframe }));
+
+    joint.position.set(x, y, z);
+    return joint;
   }
 }
 
