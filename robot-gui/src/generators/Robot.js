f1e780f0a2a98d547df1d526bf9bab368317acbd
Sebastian Florian <rebardesigndk@gmail.com>
2017-12-05 22:48:18 +0100

    feature(arm): Finish arm and controller

--- robot-gui/src/generators/Robot.js
+++ robot-gui/src/generators/Robot.js
@@ -11,15 +11,29 @@ class Robot {
     this.thickness = 1;
 
     // test only
-    const cube = this.createBox(0, 0, 5, 1.5, 0.3, 5);
-    const base = this.createJoint(0, 0, 1, 2, 2, 2, 0, 0, 0);
-    const shoulder = this.createJoint(0, 0, 8, 1, 4, 2, 0, 0, 0);
+    const base = this.createJoint(0, 0, 1, 3, 2, 2, 0, 0, 0);
+    const baseLink = this.createBox(0, 0, 3.5, 2.5, 1, 2);
+    const shoulder = this.createJoint(0, 0, 5, 1, 4, 2, 0, 0, 0);
+    const shoulderLink = this.createBox(0, 0, 7.5, 2, 0.5, 4);
+    const elbow = this.createJoint(0, 0, 10, 1, 4, 2, 0, 0, 0);
+    const elbowLink = this.createBox(0, 2.5, 10, 2, 0.5, 4);
+    const wrist = this.createJoint(-1.5, 3, 10, 0.5, 4, 2, 0, 0, 0);
+    const tip = this.createTip(0, 5, 10);
+
     base.rotation.x = (Math.PI / 2);
     shoulder.rotation.z = (Math.PI / 2);
+    elbow.rotation.z = (Math.PI / 2);
+    elbowLink.rotation.x = (Math.PI / 2);
+    wrist.rotation.z = (Math.PI / 2);
 
-    this.group.add(cube);
     this.group.add(base);
+    this.group.add(baseLink);
     this.group.add(shoulder);
+    this.group.add(shoulderLink);
+    this.group.add(elbow);
+    this.group.add(elbowLink);
+    this.group.add(wrist);
+    this.group.add(tip);
   }
 
   createBox(x = 0, y = 0, z = 0, w = 1, h = 1, d = 1) {
@@ -46,10 +60,34 @@ class Robot {
 
   createJoint(x, y, z, radius, h, d, min, max) {
     const jointGeo1 = new THREE.CylinderGeometry(radius, radius, h, 24, 2, false, -min, (2 * Math.PI) - (max + min));
-    const joint = new THREE.Mesh(jointGeo1, new THREE.MeshBasicMaterial({ color: 0xffbb00, wireframe: this.wireframe }));
+    const material = new THREE.MeshLambertMaterial({ color: 0xffbb00, wireframe: this.wireframe });
+
+    const joint = new THREE.Mesh(jointGeo1, material);
 
     joint.position.set(x, y, z);
     return joint;
+  }
+
+  createTip(x, y, z) {
+    const tipGeo = new THREE.SphereGeometry(0.5, 4, 4);
+    const tipMaterial = new THREE.MeshBasicMaterial({ color: 0xffbb00 });
+    const tip = new THREE.Mesh(tipGeo, tipMaterial);
+
+    const tipGroup = new THREE.Group();
+    tipGroup.add(tip);
+
+    const arrowZ = new THREE.ArrowHelper(new THREE.Vector3(0, 0, 1), new THREE.Vector3(0, 0, 0), 3, 0x0000ff);
+    arrowZ.line.material.linewidth = 4;
+    tipGroup.add(arrowZ);
+    const arrowY = new THREE.ArrowHelper(new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 0), 3, 0x00ff00);
+    arrowY.line.material.linewidth = 4;
+    tipGroup.add(arrowY);
+    const arrowX = new THREE.ArrowHelper(new THREE.Vector3(1, 0, 0), new THREE.Vector3(0, 0, 0), 3, 0xff0000);
+    arrowX.line.material.linewidth = 4;
+    tipGroup.add(arrowX);
+    tipGroup.position.set(x, y, z);
+
+    return tipGroup;
   }
 }
 
